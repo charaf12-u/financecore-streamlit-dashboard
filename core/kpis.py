be8a3774cca599811c04ab36d4a540a462697d1d
charaf <charafsbai01@gmail.com>
2026-05-08 11:44:33 +0100

    Update project files: KPI logic and configuration

--- core/kpis.py
+++ core/kpis.py
@@ -13,11 +13,26 @@ def calculate_kpis(df):
 
     # --> average margin
     # marge = montant * taux_interet 
-    if "taux_interet" in df.columns:
-        df["marge"] = df["montant_eur"] * (df["taux_interet"] / 100)
-        avg_margin = df["marge"].mean()
+    mapping_taux = {
+        "depot especes": 0.02,
+        "retrait dab": 0.00,
+        "prelevement": 0.00,
+        "paiement cb": 0.00,
+        "interets": 0.03,
+        "remboursement credit": 0.08,
+        "virement international": 0.01,
+        "virement": 0.00
+    }
+    df["taux_interet_estime"] = df["categorie_nom"].map(mapping_taux)
+    df["marge_estimee"] = (
+        df["montant_eur"] * df["taux_interet_estime"]
+    )
+
+    if "taux_interet_estime" in df.columns:
+        avg_margin = df["marge_estimee"].mean()
     else:
-        avg_margin = 0
+        avg_margin = 0.0
+    
 
     return {
         "total_transactions": total_transactions,
